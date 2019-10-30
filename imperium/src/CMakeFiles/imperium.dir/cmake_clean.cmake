7c9f558899f074833bd05442a9fa3ebcbae2ae0d
jocarroza <sergiobs93@gmail.com>
2019-10-30 16:54:53 +0100

    v1.0.0 - Imperium

---
+++ imperium/src/CMakeFiles/imperium.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "imperium_autogen"
+  "CMakeFiles/imperium_autogen.dir/AutogenOldSettings.cmake"
+  "CommonBehavior.cpp"
+  "CommonBehavior.h"
+  "GenericBase.cpp"
+  "GenericBase.h"
+  "DifferentialRobot.cpp"
+  "DifferentialRobot.h"
+  "Laser.cpp"
+  "Laser.h"
+  "RCISMousePicker.cpp"
+  "RCISMousePicker.h"
+  "CMakeFiles/imperium.dir/specificworker.cpp.o"
+  "CMakeFiles/imperium.dir/specificmonitor.cpp.o"
+  "CMakeFiles/imperium.dir/home/salabeta/robocomp/classes/rapplication/rapplication.cpp.o"
+  "CMakeFiles/imperium.dir/home/salabeta/robocomp/classes/sigwatch/sigwatch.cpp.o"
+  "CMakeFiles/imperium.dir/home/salabeta/robocomp/classes/qlog/qlog.cpp.o"
+  "CMakeFiles/imperium.dir/main.cpp.o"
+  "CMakeFiles/imperium.dir/genericmonitor.cpp.o"
+  "CMakeFiles/imperium.dir/commonbehaviorI.cpp.o"
+  "CMakeFiles/imperium.dir/genericworker.cpp.o"
+  "CMakeFiles/imperium.dir/rcismousepickerI.cpp.o"
+  "CMakeFiles/imperium.dir/CommonBehavior.cpp.o"
+  "CMakeFiles/imperium.dir/GenericBase.cpp.o"
+  "CMakeFiles/imperium.dir/DifferentialRobot.cpp.o"
+  "CMakeFiles/imperium.dir/Laser.cpp.o"
+  "CMakeFiles/imperium.dir/RCISMousePicker.cpp.o"
+  "CMakeFiles/imperium.dir/imperium_autogen/mocs_compilation.cpp.o"
+  "../bin/imperium.pdb"
+  "../bin/imperium"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imperium.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ imperium/src/CMakeFiles/imperium_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "imperium_autogen"
+  "CMakeFiles/imperium_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/imperium_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/imperium_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
